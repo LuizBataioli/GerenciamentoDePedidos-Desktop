c9f45a06a167d10018579dffe440e871f3620d7e
felypeEcher <echerfelype@gmail.com>
2024-06-07 20:34:06 -0300

    commitando

---
+++ OrderDesk/src/main/java/com/mycompany/orderdesk/OrderDesk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.orderdesk;
+
+/**
+ *
+ * @author felyp
+ */
+public class OrderDesk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
